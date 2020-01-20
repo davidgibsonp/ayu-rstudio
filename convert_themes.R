b8df1aa0ccf924058b1fad6165c213944705a2de
David Gibson <dvdgbsn@gmail.com>
2020-01-19 22:28:03 -0500

    added apply themes

--- convert_themes.R
+++ convert_themes.R
@@ -1,11 +1,19 @@
-# This file converts .tmTheme files to .rstheme
-rstudioapi::removeTheme(name = 'ayu light')
-rstudioapi::removeTheme(name = 'ayu dark')
-rstudioapi::removeTheme(name = 'ayu mirage')
+library(rstudioapi)
 
-rstudioapi::convertTheme(themePath = 'tmTheme/ayu-light.tmTheme',
+# Removes themes
+removeTheme(name = 'ayu light')
+removeTheme(name = 'ayu dark')
+removeTheme(name = 'ayu mirage')
+
+# Converts .tmTheme files to .rstheme
+convertTheme(themePath = 'tmTheme/ayu-light.tmTheme',
                          outputLocation = 'rstheme/')
-rstudioapi::convertTheme(themePath = 'tmTheme/ayu-dark.tmTheme',
+convertTheme(themePath = 'tmTheme/ayu-dark.tmTheme',
                          outputLocation = 'rstheme/')
-rstudioapi::convertTheme(themePath = 'tmTheme/ayu-mirage.tmTheme',
+convertTheme(themePath = 'tmTheme/ayu-mirage.tmTheme',
                          outputLocation = 'rstheme/')
+
+# Apply themes
+applyTheme('ayu dark')
+applyTheme('ayu light')
+applyTheme('ayu mirage')
